330b79306d8ec40c54ab882dd7f35029d6899df5
Gen <jdk094602@gmail.com>
2022-11-14 21:33:23 +0300

    1st task

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// SkyPro Lesson about "if" operator
+public class Main {
+    public static void main(String[] args) {
+
+     // Task 1
+    short ageUser  = 7;
+    String claimStr = "Ваш возраст "  + ageUser + " !  "  ;
+    if (ageUser > 17)
+           claimStr +=  " Поздравляю с совершеннолетием ! " ;
+    else
+           claimStr +=  " Возраст совершеннолетия еще не наступил, терпи !  " ;
+
+        System.out.println(claimStr);
+    }
+    // Task 2
+
+    // Task 3
+    // Task 4
+
+}
