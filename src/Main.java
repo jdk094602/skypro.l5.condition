5a98de580e1e3771c747baf51c6c50af8ddc93b9
Gen <jdk094602@gmail.com>
2022-11-14 22:17:39 +0300

    2nd

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,33 @@ public class Main {
     public static void main(String[] args) {
 
      // Task 1
-    short ageUser  = 7;
-    String claimStr = "Ваш возраст "  + ageUser + " !  "  ;
-    if (ageUser > 17)
-           claimStr +=  " Поздравляю с совершеннолетием ! " ;
-    else
-           claimStr +=  " Возраст совершеннолетия еще не наступил, терпи !  " ;
+        short ageUser  = 7;
+        String claimStr = "Ваш возраст "  + ageUser + " !  "  ;
+        String claimStrFull ;
 
-        System.out.println(claimStr);
+        if (ageUser > 17)
+            claimStrFull = claimStr +  " Поздравляю с совершеннолетием ! " ;
+        else
+            claimStrFull = claimStr +   " Возраст совершеннолетия еще не наступил, терпи !  " ;
+
+        System.out.println(claimStrFull);
+
+        // Task 2
+        ageUser  = 28;
+
+        claimStrFull   = "Ваш возраст - "  + ageUser + " !  "  ;
+            if (ageUser >= 7 & ageUser <= 17)
+                claimStrFull   +=   "Надеюсь, Вы исправно ходите в школу ! " ;
+            if (ageUser > 17 & ageUser < 24)
+                claimStrFull  +=  "Надеюсь, Вы уже учитесь в  ВУЗ-е ! " ;
+            if (ageUser >= 24)
+                claimStrFull  +=  "Надеюсь, Вы успешно закончили ВУЗ и уже нашли работу ! " ;
+            if (ageUser >= 7)
+                    claimStrFull += " Если нет - поторопитесь !";
+            System.out.println(claimStrFull);
+
+        // Task 3
+        // Task 4
+
     }
-    // Task 2
-
-    // Task 3
-    // Task 4
-
 }
